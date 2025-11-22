a2cfb241fe35acdfb38e764eb09b126a1b0d6df7
GabrielaCohailaAlvaradoE <gc2022075746@virtual.upt.pe>
2025-11-21 19:21:13 -0500

    first commit

---
+++ src/main/java/com/pretamo/PrestamoLibro/PrestamoLibroApplication.java
@@ -0,0 +1,13 @@
+package com.pretamo.PrestamoLibro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamoLibroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamoLibroApplication.class, args);
+	}
+
+}
